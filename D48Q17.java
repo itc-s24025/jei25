ab8249ae768ddbb4a1133b9d19f6e966039faacb
itc-s24025 <s24025@std.it-college.ac.jp>
2025-05-16 11:35:56 +0900

    課題2

---
+++ D48Q17.java
@@ -0,0 +1,11 @@
+public class D48Q17 {
+    public static void main(String[] args) {
+        int n = 35;
+        System.out.println(n > 15 && n >30);
+    }
+
+    /*
+    D48Q17について
+    Dは第48回のことを表しています
+     */
+}
