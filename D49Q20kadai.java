b29ad5428e8305b09e434eaefb48457c0e94220a
itc-s24025 <s24025@std.it-college.ac.jp>
2025-05-23 10:55:38 +0900

    D49Q18 & D49Q18kadai提出

---
+++ D49Q20kadai.java
@@ -0,0 +1,12 @@
+public class D49Q20kadai {
+    public static void main(String[] args) {
+//        double [][] arr = new double[5][10];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("[" + i + "][" + j + "] ");
+            }
+            System.out.println();
+        }
+    }
+}
