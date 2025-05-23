d2f6765039a870e271a0ba30147c09a18faa526e
itc-s24025 <s24025@std.it-college.ac.jp>
2025-05-23 12:31:35 +0900

    D49q21まで終了

---
+++ D49Q21.java
@@ -0,0 +1,8 @@
+public class D49Q21 {
+    public static void main(String[] args) {
+        int n1 = 0100;   // 8進数 10進数で64
+        int n2 = 0x10;  // 16進数 10進数で16
+
+        System.out.println(n1 + n2);
+    }
+}
