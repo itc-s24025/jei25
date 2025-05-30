16715cf46eba8a10a7156ef4172b756e36ecb428
itc-s24025 <s24025@std.it-college.ac.jp>
2025-05-30 11:21:38 +0900

    D50Q18kadai, D50Q18 課題提出

---
+++ D50Q18kadai.java
@@ -0,0 +1,8 @@
+public class D50Q18kadai {
+    public static void main(String[] args) {
+        int num = 0;
+        while (num++ < 10) {
+            System.out.println(num);
+        }
+    }
+}
---
+++ D50Q19.java
@@ -0,0 +1,6 @@
+public class D50Q19 {
+    public static void main(String[] args) {
+        double num1 = 16, num2 = 5;
+        System.out.println(num1 / num2 + ", " + num1 % num2);
+    }
+}
