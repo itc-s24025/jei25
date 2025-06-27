43e0c5e5f9d573e38ead5cbfa663ff467d654d67
itc-s24025 <s24025@std.it-college.ac.jp>
2025-06-27 12:27:11 +0900

    D51Q23kadai 提出

---
+++ D51Q23kadai.java
@@ -0,0 +1,9 @@
+public class D51Q23kadai {
+    public static void main(String[] args) {
+        // 拡張for文を使って「春。夏。秋。冬。」と出力するプログラムを書いてください
+        String[] season = {"春", "夏", "秋", "冬"};
+        for (String s : season) {
+            System.out.print(s + "。");
+        }
+    }
+}
