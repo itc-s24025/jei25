4b89fe04e541599129efa1258129faacf80f8988
itc-s24025 <s24025@std.it-college.ac.jp>
2025-06-27 10:33:41 +0900

    D51問題 動作確認

---
+++ D51Q18.java
@@ -0,0 +1,8 @@
+public class D51Q18 {
+    public  static void main(String[] args) {
+        int i;
+        for (i = 0; i < 9; i += 2) {
+            System.out.print(++i);
+        }
+    }
+}
---
+++ D51Q21.java
@@ -0,0 +1,6 @@
+public class D51Q21 {
+    public static void main(String[] args) {
+        int a = 021, b = 0x21;
+        System.out.println(a + "+" + b + "=" + (a + b));
+    }
+}
