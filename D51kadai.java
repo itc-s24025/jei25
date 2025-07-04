510d9906b935c787faef82d9b38f1d7a0d3542c2
itc-s24025 <s24025@std.it-college.ac.jp>
2025-07-04 11:29:24 +0900

    D51kadai 提出

---
+++ D51kadai.java
@@ -0,0 +1,7 @@
+public class D51kadai {
+    public static void main(String[] args) {
+        double num = Double.parseDouble(args[1]);
+        double result = Math.ceil(num);
+        System.out.println(result);
+    }
+}
