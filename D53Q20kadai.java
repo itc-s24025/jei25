209f167f7ad9be2b3d7072e33dd6769b3d2c5c1d
itc-s24025 <s24025@std.it-college.ac.jp>
2025-08-01 10:06:39 +0900

    課題提出

---
+++ D53Q20kadai.java
@@ -0,0 +1,9 @@
+public class D53Q20kadai {
+    public static void main(String[] args) {
+        int i = 8;
+        float f = 3.27f;
+
+        float n = i + f;
+        System.out.println(n);
+    }
+}
