307a52f163092f9e137ad9604a3dd3ff0405998b
itc-s24025 <s24025@std.it-college.ac.jp>
2025-08-15 14:00:02 +0900

    課題提出 修正

--- D51Q21kadai.java
+++ D51Q21kadai.java
@@ -1,8 +1,9 @@
 public class D51Q21kadai {
     public static void main(String[] args) {
-        int binary = 0b1010;
-        int decimal = 10;
+        int binary = 0b1010;    // 先頭に0bを付けて2進数を表現
+        int decimal = 10;       // 10進数の値
 
+        // 2進数の値を10進数に変換して出力
         System.out.println(binary + decimal);
     }
 }
