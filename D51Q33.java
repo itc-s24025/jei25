e5af136a1abd7341dd41494953ce506ba160bf9e
itc-s24025 <s24025@std.it-college.ac.jp>
2025-08-15 15:08:22 +0900

    課題提出

--- D51Q33.java
+++ D51Q33.java
@@ -1,26 +1,27 @@
-public class D52Q6 {
+public class D51Q33 {
     public static void main(String[] args) {
         Item[] li = { new Item("AA", "魚類"), new Item("BB", "肉類")};
         Item[] si = { new Item("A1", "さば"), new Item("A2", "さんま"),
                 new Item("B1", "牛肉"), new Item("B2", "鶏肉"),
-                new Item("B3", "豚肉") };
+                new Item("B3", "豚肉"),
+                new Item("B4", "加工肉")};
     ItemMaster im = new ItemMaster(li, si);
     String lcode = args[0].substring(0, 2);
     String scode = args[0].substring(2, 4);
     String details = args[0].substring(4);
 
-//    String lcode = im.getItemName(im.MAJOR, lcode); // 大分類コードから名前を取り出す
-//    String scode = im.getItemName(im.MINOR, scode); // 小分類コードから名前を取り出す
+    String lname = im.getItemName(im.MAJOR, lcode); // 大分類コードから名前を取り出す
+    String sname = im.getItemName(im.MINOR, scode); // 小分類コードから名前を取り出す
     System.out.println("商品コード" + args[0]);
-    System.out.println("大分類名" + lcode);
-    System.out.println("小分類名" + scode);
+    System.out.println("大分類名" + lname);
+    System.out.println("小分類名" + sname);
     System.out.println("詳細コード" + details);
     }
 }
 class Item{ // 項目を記録するクラス
     private String code;
     private String name;
-    Item(String name, String code){
+    Item(String code, String name){
         this.code = code;
         this.name = name;
     }
