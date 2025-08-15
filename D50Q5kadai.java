4c86ef7e67ef9187fe9f7b5987e5f02c4cca58b0
itc-s24025 <s24025@std.it-college.ac.jp>
2025-08-15 12:18:50 +0900

    課題提出

---
+++ D50Q5kadai.java
@@ -0,0 +1,25 @@
+public class D50Q5kadai {
+    public static void main(String[] args) {
+        int[] num = new int[args.length];
+
+        for (int i = 0; i < num.length; i++) {
+            num[i] = Integer.parseInt(args[i]);
+        }
+
+        int tmp;
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i -1; j >= 0 && num[j] > num[j + 1]; j--) {
+                tmp = num[j];
+                num[j] = num[j + 1];
+                num[j + 1] = tmp;
+            }
+            for (int val : num) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+        for (int val : num) {
+            System.out.print(val + " ");
+        }
+    }
+}
